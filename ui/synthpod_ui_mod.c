fb0ccbef79a2b676791005e598754ac0a95198c5
Hanspeter Portner <dev@open-music-kontrollers.ch>
2016-05-02 13:07:00 +0200

    make Gtk/Qt/X11/Eo UI precede concurrent show/ext.
    For plugins which define ui:showInterface on an ui:Gtk/Qt/X11, load them
    in their native UI instead of the ui:showInterface.

--- ui/synthpod_ui_mod.c
+++ ui/synthpod_ui_mod.c
@@ -836,60 +836,6 @@ _sp_ui_mod_add(sp_ui_t *ui, const char *uri, u_id_t uid)
 			mod_ui_type_t mod_ui_type = MOD_UI_TYPE_UNSUPPORTED;
 			const mod_ui_driver_t *mod_ui_driver = NULL;
 
-			// test for EoUI (precedes MOD_UI_TYPE_SANDBOX_EFL)
-			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
-			{
-				if(lilv_ui_is_a(lui, ui->regs.ui.eo.node))
-				{
-					//printf("has EoUI\n");
-#if defined(SANDBOX_LIB) && defined(SANDBOX_EFL)
-					mod_ui_type = MOD_UI_TYPE_SANDBOX_EFL;
-					mod_ui_driver = &sbox_ui_driver;
-#else
-					mod_ui_type = MOD_UI_TYPE_EFL;
-					mod_ui_driver = &efl_ui_driver;
-#endif
-				}
-			}
-
-			// test for show UI (precedes MOD_UI_TYPE_SANDBOX_SHOW)
-			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
-			{
-				//const bool has_idle_iface = lilv_world_ask(ui->world, ui_uri_node,
-				//	ui->regs.core.extension_data.node, ui->regs.ui.idle_interface.node);
-				const bool has_show_iface = lilv_world_ask(ui->world, ui_uri_node,
-					ui->regs.core.extension_data.node, ui->regs.ui.show_interface.node);
-
-				if(has_show_iface)
-				{
-					//printf("has show UI\n");
-#if defined(SANDBOX_LIB) && defined(SANDBOX_SHOW)
-					mod_ui_type = MOD_UI_TYPE_SANDBOX_SHOW;
-					mod_ui_driver = &sbox_ui_driver;
-#else
-					mod_ui_type = MOD_UI_TYPE_SHOW;
-					mod_ui_driver = &show_ui_driver;
-#endif
-				}
-			}
-
-			// test for kxstudio kx_ui (precedes MOD_UI_TYPE_SANDBOX_KX)
-			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
-			{
-				if(  lilv_ui_is_a(lui, ui->regs.ui.kx_widget.node)
-					|| lilv_ui_is_a(lui, ui->regs.ui.external.node) )
-				{
-					//printf("has kx-ui\n");
-#if defined(SANDBOX_LIB) && defined(SANDBOX_KX)
-					mod_ui_type = MOD_UI_TYPE_SANDBOX_KX;
-					mod_ui_driver = &sbox_ui_driver;
-#else
-					mod_ui_type = MOD_UI_TYPE_KX;
-					mod_ui_driver = &kx_ui_driver;
-#endif
-				}
-			}
-
 #if defined(SANDBOX_LIB)
 #	if defined(SANDBOX_X11)
 			// test for X11UI
@@ -956,6 +902,60 @@ _sp_ui_mod_add(sp_ui_t *ui, const char *uri, u_id_t uid)
 			}
 #	endif
 #endif
+
+			// test for EoUI (precedes MOD_UI_TYPE_SANDBOX_EFL)
+			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
+			{
+				if(lilv_ui_is_a(lui, ui->regs.ui.eo.node))
+				{
+					//printf("has EoUI\n");
+#if defined(SANDBOX_LIB) && defined(SANDBOX_EFL)
+					mod_ui_type = MOD_UI_TYPE_SANDBOX_EFL;
+					mod_ui_driver = &sbox_ui_driver;
+#else
+					mod_ui_type = MOD_UI_TYPE_EFL;
+					mod_ui_driver = &efl_ui_driver;
+#endif
+				}
+			}
+
+			// test for show UI (precedes MOD_UI_TYPE_SANDBOX_SHOW)
+			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
+			{
+				//const bool has_idle_iface = lilv_world_ask(ui->world, ui_uri_node,
+				//	ui->regs.core.extension_data.node, ui->regs.ui.idle_interface.node);
+				const bool has_show_iface = lilv_world_ask(ui->world, ui_uri_node,
+					ui->regs.core.extension_data.node, ui->regs.ui.show_interface.node);
+
+				if(has_show_iface)
+				{
+					//printf("has show UI\n");
+#if defined(SANDBOX_LIB) && defined(SANDBOX_SHOW)
+					mod_ui_type = MOD_UI_TYPE_SANDBOX_SHOW;
+					mod_ui_driver = &sbox_ui_driver;
+#else
+					mod_ui_type = MOD_UI_TYPE_SHOW;
+					mod_ui_driver = &show_ui_driver;
+#endif
+				}
+			}
+
+			// test for kxstudio kx_ui (precedes MOD_UI_TYPE_SANDBOX_KX)
+			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
+			{
+				if(  lilv_ui_is_a(lui, ui->regs.ui.kx_widget.node)
+					|| lilv_ui_is_a(lui, ui->regs.ui.external.node) )
+				{
+					//printf("has kx-ui\n");
+#if defined(SANDBOX_LIB) && defined(SANDBOX_KX)
+					mod_ui_type = MOD_UI_TYPE_SANDBOX_KX;
+					mod_ui_driver = &sbox_ui_driver;
+#else
+					mod_ui_type = MOD_UI_TYPE_KX;
+					mod_ui_driver = &kx_ui_driver;
+#endif
+				}
+			}
 
 			if(mod_ui_type == MOD_UI_TYPE_UNSUPPORTED)
 				continue;
